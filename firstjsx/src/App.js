0ec509e6096711cad18706142014245f0a006c78
Hoda Shobaki <shobkihoda@gmail.com>
2021-07-13 00:27:46 +0300

    first JSX

---
+++ firstjsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Hello Dojo!</h1>
+      <div>
+      <h3>Things Ineed to do</h3>
+    <ul>
+        <li>Learn React</li>
+        <li>Climb Mount Everest</li>
+        <li>Run A Marathon</li>
+        <li>Feed The Dogs</li>
+      </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
