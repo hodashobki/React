0c08805d5e6e334fcd78445e50ab55aa3f6c9a3a
Hoda Shobaki <shobkihoda@gmail.com>
2021-07-13 16:53:49 +0300

    update

--- propitup/src/components/NewComponent.js
+++ propitup/src/components/NewComponent.js
@@ -1,12 +1,23 @@
 import React, { Component} from 'react';
 
 class NewComponent extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+    increase=()=>{
+        this.setState({ age:this.state.age+1 });
+    }
 render(){
     return(
         <div>
             <h1> { this.props.firstName }, { this.props.lastName }</h1>
-            <p>Age {this.props.age}</p>
+            <p>Age {this.state.age}</p>
             <p>Hair Color: {this.props.hair}</p>
+            <button onClick={ this. increase }>Birthday of { this.props.lastName } { this.props.firstName }  </button>
+            <hr></hr>
         </div>
     )
 }
