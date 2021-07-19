be03571371222dcde10baeb1b64bbab6fa3ad29e
Hoda Shobaki <shobkihoda@gmail.com>
2021-07-19 22:55:29 +0300

    To do List

---
+++ todolist/src/components/ShowTask.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ShowTask=(props)=>{
+    return (
+        <div></div>
+    );
+}
+export default ShowTask;
---
+++ todolist/src/components/TaskForm.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+ const TaskForm=(props)=>{
+     const tasks=["Python Black belt","java Black belt","Mern Black belt"];
+     const[task,setTask]=useState("");
+    const [newtask,setNewtask]=useState([]);
+    
+    const handeldelete=(del)=>{
+const filternewtask=newtask.filter((task,j)=>{
+    return j!==del;
+});
+setNewtask(filternewtask);
+    } 
+    
+    const handelsubmit=(e)=>{
+        e.preventDefault();
+        if(task.length===0){
+            return;
+        }
+        const todoItem={
+            text:task,
+            complete:false,
+        }
+        setNewtask(...newtask,todoItem);
+        setTask("");
+       
+    }
+
+    const  handelToggleComplete=(idx)=>{
+        const updatedTodo=newtask.map((task,j)=>{
+            if(idx===j){task.complete=!task.complete;}
+        });
+        setNewtask(updatedTodo);
+    }
+    // const handelNewtask=(task)=>{
+    //     setNewtask(newtask.concat(task))
+    // }
+
+     return(
+         <div>
+          <form onSubmit={(e)=>{handelsubmit(e)}}>
+              <input type="text" onChange={(e)=>{setTask(e.target.value)}} value={task} />
+              <select value={task} onChange={(e)=>setTask(e.target.value)}>
+                  {tasks.map((task,i)=>
+                  <option key={i} value={task}>{task}</option>
+                  )}
+              </select>
+              <button>Add</button>
+          </form>
+          <>
+             {newtask.map((task,j)=>{
+                 const todoClass=[];
+                 if(task.complete){
+                     todoClass.push("line_through")
+                 }
+             return(
+                 <div key={j}>
+                     <input onChange={(e)=>{
+                       handelToggleComplete(j);
+                     }} checked={task.complete} type="checkbox"/>
+                     <span className={todoClass}>{task.text}</span>
+                     <button onClick={(e)=>{handeldelete(j)}}>Delete</button>
+                 </div>
+             );
+         })}
+         </>
+         </div>
+     );
+ }
+ export default TaskForm;
