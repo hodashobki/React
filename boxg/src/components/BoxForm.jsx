73807160da1b43b39aadfedbb5417f641d555224
Hoda Shobaki <shobkihoda@gmail.com>
2021-07-21 23:00:00 +0300

    box generator2

--- boxg/src/components/BoxForm.jsx
+++ boxg/src/components/BoxForm.jsx
@@ -4,10 +4,11 @@ const [color, setColor]=useState("");
  const handelSubmit=(e)=>{
      e.preventDefault();
      props.handel(color);
+     setColor("")
  }
      return(
          <form onSubmit={ handelSubmit}>
-          <input type="text"  name="color" onChange={(e)=>{setColor(e.target.value)}} />
+          <input type="text"  name="color" onChange={(e)=>{setColor(e.target.value)}} value={color} />
           <button>Add color</button>
          </form>
      );
