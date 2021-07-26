ca8f1f76b1689926d4eeeffb805dced8a221136a
Hoda Shobaki <shobkihoda@gmail.com>
2021-07-26 15:51:55 +0300

    APIWalker

--- apiwalker/src/Component/SearchResult.jsx
+++ apiwalker/src/Component/SearchResult.jsx
@@ -3,6 +3,17 @@ import axios from 'axios';
 
 
 const SearchResult = (props) => {
+
+const chickif=()=>{
+    if (selected=="people"){
+
+    }}
+    const chickifplanet=()=>{
+     if(selected=="planets"){
+
+    }
+}
+
     const {selected,id}=props;
     const [item, setItem]=useState("")
     useEffect(()=>{
@@ -16,12 +27,31 @@ const SearchResult = (props) => {
     }, [id]); 
     return (
         <div>
-           <p>tttt</p> 
+           <p>Star Wars</p> 
            {item.name}
-           
-           {/* {selected.length > 0 && selected.map((person, index)=>{
-            return (<div key={index}>{person.name}</div>)
-        })} */}
+           {/* var beverage = (age >= 21) ? "Beer" : "Juice"; */} 
+             {/* {selected}==="pepole"? 
+            <div>
+               <p>{item.hair_color}</p>
+           </div>:
+           <div>
+               <p>{item.rotation_period}</p>
+           </div> */}
+            {chickif} <div>
+               <p>Hair Color: {item.hair_color}</p>
+               <p>Height: {item.height}</p>
+               <p>Birthday year : {item.birth_year}</p>
+               <p> Eye Color : {item.eye_color}</p>
+           </div> 
+           {chickifplanet}
+           <div>
+               <p> {item.rotation_period}</p>
+               <p> {item.gravity}</p>
+               <p> {item.climate}</p>
+               <p> {item.diameter}</p>
+           </div> 
+         
+        
         </div>
     );
 }
